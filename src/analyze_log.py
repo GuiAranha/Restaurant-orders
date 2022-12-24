ad41e96122d84a16a5d1910dbcab9828eabad406
Guilherme Aranha <guilherme.aranha.ga@gmail.com>
2022-12-24 13:58:18 -0300

    feat: create analyze_log def

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,2 +1,34 @@
+import csv
+from src.track_orders import TrackOrders
+
+
+def get_orders(path_to_file):
+    if '.csv' not in path_to_file:
+        raise FileNotFoundError(f"Extensão inválida: {path_to_file}")
+    try:
+        with open(path_to_file, "rt") as f:
+            return [line for line in csv.reader(f)]
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: {path_to_file}")
+
+
 def analyze_log(path_to_file):
-    raise NotImplementedError
+    track_orders = TrackOrders()
+    orders = get_orders(path_to_file)
+    for client, dish, day in orders:
+        track_orders.add_new_order(client, dish, day)
+
+    most_ordered_dish = track_orders.get_most_ordered_dish_per_customer(
+        "maria"
+    )
+    times_ordered = track_orders.count_orders(orders, "arnaldo", "hamburguer")
+    never_ordered = track_orders.get_never_ordered_per_customer("joao")
+    never_visited = track_orders.get_days_never_visited_per_customer("joao")
+
+    with open("data/mkt_campaign.txt", "w") as f:
+        f.write(
+            f"{most_ordered_dish}\n"
+            f"{times_ordered}\n"
+            f"{never_ordered}\n"
+            f"{never_visited}"
+        )
